c4dbadce462458ea2f960c0f8d91e90e74919971
Vinicius J. F <www.vini84200@hotmail.com>
2019-06-16 19:57:42 -0300

    Update urls.py

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from feedback.views import FeedbackView
 
-urlpatterns = [
-        url(r'^ajax(?P<url>.*)$', FeedbackView.as_view(), name='feedback'),
+urlpatterns = \
+    [
+        path('ajax<url>', FeedbackView.as_view(), name='feedback'),
     ]
 
 # vim: et sw=4 sts=4
